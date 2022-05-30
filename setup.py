6a58688fb2acf780c516a457e1ebbfbfac1278db
Jordi Petit <jpetit@cs.upc.edu>
2022-05-30 15:24:56 +0200

    Fix typing stubs

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='yogi',
     packages=['yogi'],
     install_requires=[''],
-    package_data={"yogi": ["yogi.pyi"]},
+    package_data={"yogi": ["__init__.pyi", "py.typed"]},
     version=version,
     description='Simple typed interface to read input in Python',
     long_description='Simple typed interface to read input in Python',
